375d33426d317ad4f20c93f1803066294e0b1343
Gotchas99 <Gotchas99@users.noreply.github.com>
2017-01-21 11:10:02 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ public class Program
     public static void Main()
     {
         new WebHostBuilder()
-            .UseUrls("http://*:5000/")
+            //.UseUrls("http://*:5000/")
             .UseKestrel()
             .Configure(a => a.Run(c => c.Response.WriteAsync("Hi there!")))
             .Build()
